17c7304bcfdbb8a131107e1dfa27bbddf5691ce6
Khagan <khaqan.rustamov@gmail.com>
2023-12-10 15:50:30 +0400

    some fixes

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,10 +1,8 @@
-"use client";
-
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
 
 interface ThemeContextProps {
   toggle: () => void;
-  mode: string | null;
+  mode: string;
 }
 
 interface ThemeProviderProps {
@@ -13,17 +11,24 @@ interface ThemeProviderProps {
 
 export const ThemeContext = createContext<ThemeContextProps>({
   toggle: () => {},
-  mode: null,
+  mode: "",
 });
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const [mode, setMode] = useState(() => localStorage.getItem("theme"));
+  const [mode, setMode] = useState(() => {
+    const storedMode = localStorage.getItem("theme");
+    return storedMode !== null ? storedMode : "";
+  });
 
   const toggle = () => {
     const newMode = mode === "dark" ? "light" : "dark";
     setMode(newMode);
     localStorage.setItem("theme", newMode);
   };
+
+  useEffect(() => {
+    localStorage.setItem("theme", mode);
+  }, [mode]);
 
   return (
     <ThemeContext.Provider value={{ toggle, mode }}>
