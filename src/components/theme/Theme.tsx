62b2c4051108529b3e00086979d452c6de6bb028
Khagan <khaqan.rustamov@gmail.com>
2023-12-10 18:32:08 +0400

    some fixes

--- src/components/theme/Theme.tsx
+++ src/components/theme/Theme.tsx
@@ -20,5 +20,5 @@ const Theme = () => {
     </div>
   );
 };
-
+  
 export default Theme;
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -17,12 +17,23 @@ export const ThemeContext = createContext<ThemeContextProps>({
 });
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const [mode, setMode] = useState(() => localStorage.getItem("theme"));
+  const [mode, setMode] = useState(() => {
+    try {
+      return localStorage.getItem("theme") || "light";
+    } catch (error) {
+      console.error("Error getting theme from localStorage:", error);
+      return "light";
+    }
+  });
 
   const toggle = () => {
     const newMode = mode === "dark" ? "light" : "dark";
     setMode(newMode);
-    localStorage.setItem("theme", newMode);
+    try {
+      localStorage.setItem("theme", newMode);
+    } catch (error) {
+      console.error("Error setting theme in localStorage:", error);
+    }
   };
 
   return (
